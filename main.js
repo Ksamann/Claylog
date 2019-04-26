7b098b91a918471659f01143209aceb1aa30f655
Ksamann <39223188+Ksamann@users.noreply.github.com>
2019-04-26 14:53:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
 var numberOfPlates = 5;
+var shotsPerPlate = 5;
+
 
 
 
@@ -7,10 +9,23 @@ var numberOfPlates = 5;
 function makeScore(){
 
     for(var i = 0; i < numberOfPlates; i++ ){
-        var btn1 = document.createElement("button");
-    btn1.className ="shot";
+
+    var plate = document.createElement("section");
+    plate.className = "plate";
+    var plateText = document.createElement("p");
+    plateText.className = "plateText";
+    plateText.innerHTML = "Plate " + (i+1);
+    plate.appendChild(plateText);
+
+        for(var j = 0; j < shotsPerPlate; j++){
+            var btn1 = document.createElement("button");
+            btn1.className ="shot";
+            plate.appendChild(btn1);
+        }
+
+   
     var score = document.getElementById("score");
-    score.appendChild(btn1);
+    score.appendChild(plate);
 
     }
 
